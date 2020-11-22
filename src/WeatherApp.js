789473f2ea303c30cbbdc96780c2bdddac9ef497
蒼語 <72489589+abb4119@users.noreply.github.com>
2020-11-22 23:55:53 +0800

    Initial commit

---
+++ src/WeatherApp.js
@@ -0,0 +1,181 @@
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+import { ReactComponent as CloudyIcon } from "./images/cloudy.svg";
+import { ReactComponent as AirFlowIcon } from "./images/airFlow.svg";
+import { ReactComponent as RainIcon } from "./images/rain.svg";
+import { ReactComponent as RedoIcon } from "./images/redo.svg";
+
+const Container = styled.div`
+  background-color: #000000;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const WeatherCard = styled.div`
+  position: relative;
+  min-width: 360px;
+  box-shadow: 0 1px 3px 0 #000000;
+  background-color: #d0d0d0;
+  box-sizing: border-box;
+  padding: 30px 15px;
+`;
+
+const Location = styled.div`
+  font-size: 50px;
+  color: #2f0000;
+  margin-bottom: 20px;
+`;
+
+const Description = styled.div`
+  font-size: 20px;
+  color: #000000;
+  margin-bottom: 30px;
+`;
+
+const CurrentWeather = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 30px;
+`;
+
+const Temperature = styled.div`
+  color: #ff0000;
+  font-size: 96px;
+  font-weight: 300;
+  display: flex;
+`;
+
+const Celsius = styled.div`
+  font-weight: normal;
+  font-size: 42px;
+`;
+
+const Cloudy = styled(CloudyIcon)`
+  flex-basis: 30%;
+`;
+
+const AirFlow = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: #272727;
+  margin-bottom: 20px;
+
+  svg {
+    width: 25px;
+    height: auto;
+    margin-right: 30px;
+  }
+`;
+
+const Rain = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: #272727;
+
+  svg {
+    width: 25px;
+    height: auto;
+    margin-right: 30px;
+  }
+`;
+
+const Redo = styled.div`
+  position: absolute;
+  right: 15px;
+  bottom: 15px;
+  font-size: 12px;
+  display: inline-flex;
+  align-items: flex-end;
+  color: #272727;
+
+  svg {
+    margin-left: 10px;
+    width: 15px;
+    height: 15px;
+    cursor: pointer;
+  }
+`;
+
+const WeatherApp = () => {
+  const [currentWeather, setCurrentWeather] = useState({
+    observationTime: "2019-10-02 22:10:00",
+    locationName: "臺北市",
+    description: "多雲時晴",
+    temperature: 27.5,
+    windSpeed: 0.3,
+    humid: 0.88
+  });
+
+  const handleClick = () => {
+    fetch(
+      "https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=CWB-507B37E0-0383-4D8C-878D-628B54EC3536&locationName=臺北"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        // STEP 1：定義 `locationData` 把回傳的資料中會用到的部分取出來
+        const locationData = data.records.location[0];
+
+        // STEP 2：將風速（WDSD）、氣溫（TEMP）和濕度（HUMD）的資料取出
+        const weatherElements = locationData.weatherElement.reduce(
+          (neededElements, item) => {
+            if (["WDSD", "TEMP", "HUMD"].includes(item.elementName)) {
+              neededElements[item.elementName] = item.elementValue;
+            }
+            return neededElements;
+          },
+          {}
+        );
+
+        // STEP 3：要使用到 React 組件中的資料
+        setCurrentWeather({
+          observationTime: locationData.time.obsTime,
+          locationName: locationData.locationName,
+          description: "午後偶陣雨",
+          temperature: weatherElements.TEMP,
+          windSpeed: weatherElements.WDSD,
+          humid: weatherElements.HUMD
+        });
+      });
+  };
+
+  return (
+    <Container>
+      <WeatherCard>
+        <Location>{currentWeather.locationName}</Location>
+        <Description>{currentWeather.description}</Description>
+        <CurrentWeather>
+          <Temperature>
+            {Math.round(currentWeather.temperature)} <Celsius>°C</Celsius>
+          </Temperature>
+          <Cloudy />
+        </CurrentWeather>
+        <AirFlow>
+          <AirFlowIcon />
+          {currentWeather.windSpeed} m/h
+        </AirFlow>
+        <Rain>
+          <RainIcon />
+          {Math.round(currentWeather.humid * 100)} %
+        </Rain>
+
+        <Redo onClick={handleClick}>
+          最後觀測時間：
+          {new Intl.DateTimeFormat("zh-TW", {
+            hour: "numeric",
+            minute: "numeric"
+          }).format(new Date(currentWeather.observationTime))}{" "}
+          <RedoIcon />
+        </Redo>
+      </WeatherCard>
+    </Container>
+  );
+};
+
+export default WeatherApp;
